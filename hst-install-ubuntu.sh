df1ffef59059b571482a0f52fa370ef9e08f252e
phucvietdhv <93958203+phucvietdhv@users.noreply.github.com>
2021-11-09 16:50:04 +0700

    Update hst-install-ubuntu.sh

--- hst-install-ubuntu.sh
+++ hst-install-ubuntu.sh
@@ -19,12 +19,12 @@ HESTIA_INSTALL_DIR="$HESTIA/install/deb"
 VERBOSE='no'
 
 # Define software versions
-HESTIA_INSTALL_VER='1.4.17'
-pma_v='4.9.7'
-rc_v="1.4.11"
+HESTIA_INSTALL_VER='1.5.0~beta'
+pma_v='5.1.1'
+rc_v="1.5.0"
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
 fpm_v="5.6"
-mariadb_v="5.5"
+mariadb_v="10.6"
 
 # Defining software pack for all distros
 software="apache2 apache2.2-common apache2-suexec-custom apache2-utils
