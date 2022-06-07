0f23e84bebf301e4ce3bd11849f7715dc45b12fd
phucvietdhv <93958203+phucvietdhv@users.noreply.github.com>
2022-06-07 15:59:07 +0700

    Update hst-install.sh

--- hst-install.sh
+++ hst-install.sh
@@ -78,7 +78,7 @@ fi
 check_wget_curl(){
     # Check wget
     if [ -e '/usr/bin/wget' ]; then
-        wget -q https://raw.githubusercontent.com/phucvietdhv/hestiacpinstall/main/hst-install-$type.sh -O hst-install-$type.sh
+        wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh -O hst-install-$type.sh
         if [ "$?" -eq '0' ]; then
             bash hst-install-$type.sh $*
             exit
@@ -90,7 +90,7 @@ check_wget_curl(){
 
     # Check curl
     if [ -e '/usr/bin/curl' ]; then
-        curl -s -O https://raw.githubusercontent.com/phucvietdhv/hestiacpinstall/main/hst-install-$type.sh
+        curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh
         if [ "$?" -eq '0' ]; then
             bash hst-install-$type.sh $*
             exit
